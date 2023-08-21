4173cfc8482b92f21bdfdd9f0a6d8ddaf6f92356
Moses Chukwunekwu <dantereus1@gmail.com>
2023-08-21 23:06:20 +0100

    Minor fixes

--- src/constants/index.js
+++ src/constants/index.js
@@ -126,8 +126,8 @@ const experiences = [
     date: "November 2022 - Present",
     points: [
       "Collaborate closely with clients to comprehend their business requirements, translating them into functional and visually appealing website designs.",
-      "Successfully manage multiple projects simultaneously, ensuring on-timedelivery and exceeding client expectations.",
-      "Maintain continuous communication and collaboration with clients, providingupdates, gathering feedback, and incorporating necessary revisions to meet their needs.",
+      "Successfully manage multiple projects simultaneously, ensuring on-time delivery and exceeding client expectations.",
+      "Maintain continuous communication and collaboration with clients, providing updates, gathering feedback, and incorporating necessary revisions to meet their needs.",
     ],
   },
   {
@@ -139,8 +139,8 @@ const experiences = [
     points: [
       "Played a key role in optimizing the performance of the frontend architecture using TypeScript, React JS, Storybook, CSS-in-JS, and GraphQL, resulting in 25% faster load times and improved user experience.",
       "Contributed significantly to developing an internal reusable component library, significantly enhancing code reusability and maintaining consistency across the project",
-      "Participated in code reviews, providing valuable feedback and suggestions forcode quality and best practices.",
-      "Actively contributed to improving team collaboration and productivity througheffective communication and knowledge sharing during pair-programming sessions",
+      "Participated in code reviews, providing valuable feedback and suggestions for code quality and best practices.",
+      "Actively contributed to improving team collaboration and productivity through effective communication and knowledge sharing during pair-programming sessions",
     ],
   },
   {
@@ -151,7 +151,7 @@ const experiences = [
     date: "June 2021- January 2022",
     points: [
       "Efficiently collaborated with Frontend teams to develop a map-data visualization program for real estate data search with server-side rendering. Demonstrated adaptability and delivered results within a short contract period.",
-      "Implemented MapBox GL JS with customizations to visualize data in grids andlists, resulting in a robust and visually appealing map-data visualization feature. This enhancement significantly improved user engagement by 30%",
+      "Implemented MapBox GL JS with customizations to visualize data in grids and lists, resulting in a robust and visually appealing map-data visualization feature. This enhancement significantly improved user engagement by 30%",
       "Collaborated seamlessly with back-end teams to ensure smooth integration ofboth frontend and backend functionalities.",
       "Performed rigorous testing and debugging to ensure a smooth user experience on diverse devices and browsers.",
     ],
@@ -320,7 +320,7 @@ const projects = [
       },
     ],
     image: neutrix,
-    source_code_link: "https://github.com/moses-py/neutrix",
+    source_code_link: "https://github.com/moses-py/neutrix-app",
     site_link: "https://neutrix-app.vercel.app/",
   },
   {
